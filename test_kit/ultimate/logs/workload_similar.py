e66965904fd56e9b312dc16500fb470d492666b3
yilun wang <49629610+wiluen@users.noreply.github.com>
2024-08-31 17:09:56 +0800

    Update workload_similar.py

--- test_kit/ultimate/logs/workload_similar.py
+++ test_kit/ultimate/logs/workload_similar.py
@@ -1,20 +1,23 @@
 import numpy as np
 from numpy.linalg import norm
 from scipy.spatial.distance import cdist
-import os
-import math
 
+model_dict={'wc.txt':['model/wc/Actor_td3.pt','model/wc/Critic1_td3.pt','model/wc/Critic2_td3.pt'],
+            'ts.txt':['model/ts/Actor_td3.pt','model/ts/Critic1_td3.pt','model/ts/Critic2_td3.pt'],
+            'pr.txt':['model/pr/Actor_td3.pt','model/pr/Critic1_td3.pt','model/pr/Critic2_td3.pt'],
+            'km.txt':['model/km/Actor_td3.pt','model/km/Critic1_td3.pt','model/km/Critic2_td3.pt']
+            }
 Alog_feature=[]
-A=[]
-All_feature=[]
+target_worklaod_feature=[]
+exist_workload_feature=[]
 similarity=[]
 file=['wc.txt','ts.txt','pr.txt','km.txt']
-with open('pca.txt') as f:
+with open('agg.txt') as f:
     seq = f.read()
     for i in range(0,16):
         character_i = str(seq.split('\n')[i])
         Alog_feature.append(float(character_i.split(':')[1]))
-A.append(Alog_feature)
+target_worklaod_feature.append(Alog_feature)
 
 for filename in file:
     with open(filename) as f:
@@ -23,10 +26,12 @@ for filename in file:
         for i in range(0, 16):
             character_i = str(seq.split('\n')[i])
             base_feature.append(float(character_i.split(':')[1]))
-        All_feature.append(base_feature)
+        exist_workload_feature.append(base_feature)
 
-print(A)
-print(All_feature)
+# print('target_worklaod_feature',target_worklaod_feature)
+# print('exist_workload_feature',exist_workload_feature)
 
-standardE =cdist(All_feature, A, metric='seuclidean')    #seuclidean
-print(standardE)
+standardE =cdist(exist_workload_feature, target_worklaod_feature, metric='seuclidean')    #seuclidean
+arr=np.array(standardE)
+min_index =np.argmin(arr)
+print('base model:',model_dict[file[min_index]])
